164baf5550d71e162c47f95a2bef675a807a8539
Vinicius <viniciusolicveira@gmail.com>
2024-08-25 19:18:41 -0300

    feat(entity): adiciona entidades Pessoa e Tarefa

--- src/main/java/com/viniciussantos/model/Pessoa.java
+++ src/main/java/com/viniciussantos/model/Pessoa.java
@@ -1,4 +1,35 @@
 package com.viniciussantos.model;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome da pessoa é obrigatório.")
+    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres.")
+    private String nome;
+
+    @NotBlank(message = "O departamento da pessoa é obrigatório.")
+    private String departamento;
+
+
+    @OneToMany(mappedBy = "pessoa")
+    private List<Tarefa> tarefas;
+
+
 }
--- src/main/java/com/viniciussantos/model/Tarefa.java
+++ src/main/java/com/viniciussantos/model/Tarefa.java
@@ -1,4 +1,47 @@
 package com.viniciussantos.model;
 
+
+import com.viniciussantos.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Tarefa {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O título da tarefa é obrigatório.")
+    private String titulo;
+
+    @NotBlank(message = "A descrição da tarefa é obrigatória.")
+    private String descricao;
+
+
+    @NotBlank(message = "O prazo da tarefa é obrigatório.")
+    private String prazo;
+
+    @NotBlank(message = "O departamento da tarefa é obrigatório.")
+    private String departamento;
+
+    @NotBlank(message = "A duração da tarefa é obrigatória.")
+    private String duracao;
+
+    @ManyToOne()
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+
+    private String status = Status.PENDENDTE.getStatus();
+
+
 }
