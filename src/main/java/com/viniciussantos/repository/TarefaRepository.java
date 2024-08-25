1d13cc0bde31774e61ddfec95613cf05c3fb6a28
Vinicius <viniciusolicveira@gmail.com>
2024-08-25 19:19:51 -0300

    feat(repository): adiciona camada de repositório para as entidades Pessoa e Tarefa

---
+++ src/main/java/com/viniciussantos/repository/TarefaRepository.java
@@ -0,0 +1,11 @@
+package com.viniciussantos.repository;
+
+
+import com.viniciussantos.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
